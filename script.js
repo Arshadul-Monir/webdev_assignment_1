ab147157b0f0a566626591f3472eba38f76158fb
Arshadul-Monir <arshadulmonir@gmail.com>
2024-06-17 22:58:05 -0400

    finished delete aesthetics

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 function add()
 {
     const element = document.getElementById("inp");
+    if(element.value == "")
+    {
+        return;
+    }
     const da_list = document.getElementById("list");
     const da_plan = document.createElement("li");
 
-    const da_delete = document.createElement("button");
-    da_delete.textContent = "deeleet";
-    da_delete.onclick = function() { del(this); };
+    // const da_delete = document.createElement("button");
+    // da_delete.className = "delete-button";
+    // da_delete.textContent = "X";
+    da_plan.onclick = function() { del(this); };
 
-    da_plan.textContent = element.value + " ";
-    da_plan.append(da_delete);
+    da_plan.textContent = element.value;
+    // da_plan.appendChild(da_delete);
     da_list.append(da_plan);
     element.value = "";
 }
 
 function del(item)
 {
-    item.parentElement.remove();
+    item.remove();
 }
