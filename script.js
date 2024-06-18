e2b5631b4cff35a613270b883b34cfb2d3477035
Arshadul-Monir <arshadulmonir@gmail.com>
2024-06-17 21:59:40 -0400

    page 3 delete

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function add(index)
+function add()
 {
-    let da_list = document.getElementById("list");
-    let da_plan = document.createElement("li");
-    da_plan.textContent = index;
+    const element = document.getElementById("inp");
+    const da_list = document.getElementById("list");
+    const da_plan = document.createElement("li");
+
+    const da_delete = document.createElement("button");
+    da_delete.textContent = "deeleet";
+    da_delete.onclick = function() { del(this); };
+
+    da_plan.textContent = element.value + " ";
+    da_plan.append(da_delete);
     da_list.append(da_plan);
+    element.value = "";
+}
+
+function del(item)
+{
+    item.parentElement.remove();
 }
